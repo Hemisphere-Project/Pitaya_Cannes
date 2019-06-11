c8b515fdf6af6bdad74eee7280392c5b353b83dc
Jeremie <jeremie@hemisphere-project.com>
2019-06-11 12:36:38 +0200

    init

---
+++ Cannes_ESP/YUMO_E6A2.ino
@@ -0,0 +1,21 @@
+
+
+
+
+void updateEncoder(){
+  int MSB = digitalRead(encoderPin1); //MSB = most significant bit
+  int LSB = digitalRead(encoderPin2); //LSB = least significant bit
+
+  int encoded = (MSB << 1) |LSB; //converting the 2 pin value to single number
+  int sum  = (lastEncoded << 2) | encoded; //adding it to the previous encoded value
+
+  if(sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011) encoderValue ++;
+  if(sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000) encoderValue --;
+
+  lastEncoded = encoded; //store this value for next time
+
+  //Constrain to 1 tour - 360°
+  if (encoderValue>799){ encoderValue = 0; }
+  if (encoderValue<0){ encoderValue = 799; }
+  encoderAngle = map(encoderValue, 0, 799, 0, 359);
+}
